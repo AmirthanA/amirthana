b252ff85cec4fe96ef61a7341afad147b0b66083
AmirthanA <amirthanaram@gmail.com>
2016-12-27 14:02:12 +0530

    Add files via upload

---
+++ power.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+main()
+{
+int power,m,n;
+printf("\n enter the number for m:");
+scanf("%d",&m);
+printf("\n enter the number for n:");
+scanf("%d",&n);
+power=pow(m,n);
+{
+printf("\n the power is ,%d", power);
+}
+}
