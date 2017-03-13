35edf5f15ec0f2cd559a9b4cb9e795d3001c6df6
AmirthanA <amirthanaram@gmail.com>
2017-03-13 15:08:08 +0530

    Add files via upload

---
+++ Untitled100.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<string.h>
+ main()
+{
+char s[20];
+printf("\n enter the char");
+scanf("%d",&s);
+if(strcmp(s,"sunday")==0)
+printf("\n  false");
+else
+printf("\n  true");
+return 0;
+}
