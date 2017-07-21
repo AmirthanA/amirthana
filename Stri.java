1d1a39997d204da86c9d81f1a26a45e8ff42034d
AmirthanA <amirthanaram@gmail.com>
2017-07-21 14:22:24 +0530

    Add files via upload

---
+++ Stri.java
@@ -0,0 +1,18 @@
+package rough;                         
+import java.util.*;
+public class Stri {
+public static void main(String args[]){
+	Scanner s=new Scanner(System.in);
+	String a[]={"sun","mon","tues","wed","thurs","fri","sat"};
+   for(int i=0;i<7;i++){
+    	if(a[i]=="sat"||a[i]=="sun"){
+    		System.out.println("false");
+    		
+    	}
+    	else{
+    		System.out.println("true");
+    	}
+  }
+}
+}
+	
